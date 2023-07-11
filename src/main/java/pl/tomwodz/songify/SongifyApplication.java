ee4f6a8bf99137901dc1531f6a11e9233519e027
twodzinski@op.pl <twodzinski@op.pl>
2023-07-11 20:56:00 +0200

    first commit

---
+++ src/main/java/pl/tomwodz/songify/SongifyApplication.java
@@ -0,0 +1,13 @@
+package pl.tomwodz.songify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongifyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SongifyApplication.class, args);
+    }
+
+}
