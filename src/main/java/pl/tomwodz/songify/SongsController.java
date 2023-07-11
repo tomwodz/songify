efac339cd0ace13d2b55172e5a5970eb3fc83fc4
twodzinski@op.pl <twodzinski@op.pl>
2023-07-11 21:58:09 +0200

    RequestHeader

--- src/main/java/pl/tomwodz/songify/SongsController.java
+++ src/main/java/pl/tomwodz/songify/SongsController.java
@@ -1,18 +1,17 @@
 package pl.tomwodz.songify;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.web.server.Cookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
+@Log4j2
 public class SongsController {
 
     Map<Integer, String> database = new HashMap<>();
@@ -43,8 +42,21 @@ public class SongsController {
         return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/songs/{id}")
+/*
+   @GetMapping("/songs/{id}")
     public ResponseEntity<SingleSongResponseDto> getAllSongById(@PathVariable Integer id){
+        String song = database.get(id);
+        if(song  == null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        SingleSongResponseDto response = new SingleSongResponseDto(song);
+        return ResponseEntity.ok(response);
+    }
+*/
+
+    @GetMapping("/songs/{id}")
+    public ResponseEntity<SingleSongResponseDto> getAllSongById(@PathVariable Integer id, @RequestHeader(required = false) String requestId){
+        log.info(requestId);
         String song = database.get(id);
         if(song  == null){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
